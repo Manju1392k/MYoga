372d9b9b5efb359e70717867a2120f5c40abf6cd
Manjunath <manjunath858535@gmail.com>
2025-02-28 19:49:08 +0530

    our-review-section-completed

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -49,22 +49,71 @@ export default function Home() {
       </div>
 
       {/* Div for Yoga Images. */}
-      <div className="yogaimages w-[100vw] py-4 maincolor flex items-center justify-around flex-wrap">
+      <div className="yogaimages w-[100vw] py-14 maincolor flex items-center justify-around flex-wrap">
 
         {/* Div for whitebox. */}
         <div className='whitebox py-3 px-4 flex flex-col bg-white rounded-sm items-center'>
-          <img className='h-[10rem]' src="/Photos/Bow.png" alt="" />
+          <img className='h-[8rem]' src="/Photos/Bow.png" alt="" />
           <h1 className='inter font-bold mt-2 text-xl'>BOW</h1>
         </div>
         {/* Div for whitebox. */}
         <div className='whitebox py-3 px-4 flex flex-col bg-white rounded-sm items-center'>
-          <img className='h-[10rem]' src="/Photos/Child.png" alt="" />
+          <img className='h-[8rem]' src="/Photos/Child.png" alt="" />
           <h1 className='inter font-bold mt-2 text-xl'>Child</h1>
         </div>
         {/* Div for whitebox. */}
         <div className='whitebox py-3 px-4 flex flex-col bg-white rounded-sm items-center'>
-          <img className='h-[10rem]' src="/Photos/Camel.png" alt="" />
+          <img className='h-[8rem]' src="/Photos/Camel.png" alt="" />
           <h1 className='inter font-bold mt-2 text-xl'>Camel</h1>
+        </div>
+
+      </div>
+
+      <h1 className='poppins text-2xl font-bold italic text-center my-2'>Our Customers Reviewers</h1>
+
+      {/* Div for Reviews. */}
+      <div className="Reviews w-[100vw] flex justify-center items-center">
+
+        {/* Div for Reviewcard. */}
+        <div className="reviewcard cardcolor w-[22rem] rounded-md py-3 px-4">
+          {/* Div for stars. */}
+          <div className="flex">
+            <img className='h-[1.2rem] ml-1' src="/Photos/Star.png" alt="" />
+            <img className='h-[1.2rem] ml-1' src="/Photos/Star.png" alt="" />
+            <img className='h-[1.2rem] ml-1' src="/Photos/Star.png" alt="" />
+            <img className='h-[1.2rem] ml-1' src="/Photos/Star.png" alt="" />
+            <img className='h-[1.2rem] ml-1' src="/Photos/Star.png" alt="" />
+          </div>
+          <h1 className='inter text-lg font-bold text-white'>~ Ravi Kumar</h1>
+          <h1 className='inter text-base text-white'>MYoga has completely transformed my fitness journey. Manjunath’s expertise and compassionate teaching style have helped me achieve flexibility and strength I never thought possible. The sense of community here is unparalleled highly recommend!</h1>
+        </div>
+
+        {/* Div for Reviewcard. */}
+        <div className="reviewcard cardcolor w-[22rem] rounded-md py-3 px-4 mx-3">
+          {/* Div for stars. */}
+          <div className="flex">
+            <img className='h-[1.2rem] ml-1' src="/Photos/Star.png" alt="" />
+            <img className='h-[1.2rem] ml-1' src="/Photos/Star.png" alt="" />
+            <img className='h-[1.2rem] ml-1' src="/Photos/Star.png" alt="" />
+            <img className='h-[1.2rem] ml-1' src="/Photos/Star.png" alt="" />
+            <img className='h-[1.2rem] ml-1' src="/Photos/Star.png" alt="" />
+          </div>
+          <h1 className='inter text-lg font-bold text-white'>~ S Vikram</h1>
+          <h1 className='inter text-base text-white'>I have been practicing yoga for years, but Manjunath’s classes at MYoga take it to the next level. His tailored approach ensures every session is engaging and effective. I feel more balanced and energized every day thanks to MYoga.</h1>
+        </div>
+
+        {/* Div for Reviewcard. */}
+        <div className="reviewcard cardcolor w-[22rem] rounded-md py-3 px-4">
+          {/* Div for stars. */}
+          <div className="flex">
+            <img className='h-[1.2rem] ml-1' src="/Photos/Star.png" alt="" />
+            <img className='h-[1.2rem] ml-1' src="/Photos/Star.png" alt="" />
+            <img className='h-[1.2rem] ml-1' src="/Photos/Star.png" alt="" />
+            <img className='h-[1.2rem] ml-1' src="/Photos/Star.png" alt="" />
+            <img className='h-[1.2rem] ml-1' src="/Photos/Star.png" alt="" />
+          </div>
+          <h1 className='inter text-lg font-bold text-white'>~ Radhika P</h1>
+          <h1 className='inter text-base text-white'>Joining MYoga has been the best decision for my physical and mental well-being. Manjunath’s passion for yoga is evident in every class, and his guidance has made a significant difference in my fitness journey. I look forward to each session.</h1>
         </div>
 
       </div>
